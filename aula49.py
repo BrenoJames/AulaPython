1e28c89682056ae7a577fd57254a2b0d789bc0d5
BrenoJames <bjmetec@gmail.com>
2026-02-27 02:34:57 -0300

    atualizaçoes das aulas

--- aula49.py
+++ aula49.py
@@ -10,13 +10,16 @@ Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
 #        0   1   2   3   4   5
 lista = [10, 20, 30, 40]
-lista[2] = 300
-del lista[2]
+#lista[2] = 300
+#del lista[2]
+#print(lista[2])
+lista.append(50)
+lista.append(60)
+lista.append(70)
+lista.append(80)
+lista.append(90)
+ultimo_valor = lista.pop(5)
 print(lista)
-print(lista[2])
-#lista.append(50)
-#lista.pop()
-#lista.append(60)
-#lista.append(70)
-#ultimo_valor = lista.pop(3)
+#lista.pop() esse aqui remove o último elemento da lista
+
 #print(lista, 'Removido,', ultimo_valor)
